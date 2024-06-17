592987c0680cf854b1d818667072e1b0ded6042a
Gerardo Flores <63570510+gflores34@users.noreply.github.com>
2024-06-16 20:34:49 -0500

    added text formatter

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from tkinter import filedialog
 import tkinter.filedialog
 import tkinter.messagebox
 import customtkinter
+from CTkMessagebox import CTkMessagebox
 from drawing_finder import drawing_finder
 from PIL import Image, ImageTk
-import fitz
-
+import os
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+class PDFViewer:
+    def __init__(self, master):
+        pass
+
+    
 
 class App(customtkinter.CTk):
 
@@ -22,7 +27,9 @@ class App(customtkinter.CTk):
 
         # configure window
         self.title("DETEX APP")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1358}x{764}")
+        self.resizable(width=False, height=False)
+        self.attributes('-topmost', True)
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
@@ -43,6 +50,10 @@ class App(customtkinter.CTk):
         #Sidebar load drawing button
         self.drawing_button = customtkinter.CTkButton(self.sidebar_frame, command=self.load_drawing, text="Drawing Finder")
         self.drawing_button.grid(row=1, column=0, padx=20, pady=50)
+
+        #Sidebar load drawing button
+        self.formatter_button = customtkinter.CTkButton(self.sidebar_frame, command=self.load_formatter, text="Text Formatter")
+        self.formatter_button.grid(row=2, column=0, padx=20, pady=0)
 
         #Settings sidebar
         settings_icon = customtkinter.CTkImage(dark_image=Image.open("./assets/icons/settings_white.png"))
@@ -68,33 +79,58 @@ class App(customtkinter.CTk):
 
 
         #Drawing Finder frame
-        self.drawing_finder_frame = customtkinter.CTkFrame(self, width=250, fg_color="blue")
+        self.drawing_finder_frame = customtkinter.CTkFrame(self, fg_color="gray12")
         self.drawing_finder_frame.grid(row=0, column=1, rowspan = 3, columnspan = 3,sticky="nsew")
-        self.drawing_finder_frame.grid_columnconfigure((0,1,2), weight=0)
+        self.drawing_finder_frame.grid_columnconfigure((0,1,2,3,4), weight=0)
         self.drawing_finder_frame.grid_rowconfigure((0,1,2,3,4,5), weight=0)
 
 
         folder_icon = customtkinter.CTkImage(dark_image=Image.open("./assets/icons/folder_white.png"))
         self.set_dir_button = customtkinter.CTkButton(self.drawing_finder_frame, text="", command= self.set_dir, width=1, image=folder_icon)
-        self.set_dir_button.grid(row=0, column=1, padx=(20,0), pady=20)
+        self.set_dir_button.grid(row=0, column=1, padx=(20,0), pady=50, sticky="n")
 
         self.drawing_dir_label = customtkinter.CTkEntry(self.drawing_finder_frame, font=("Arial", 12), width=300, textvariable=self.drawing_directory, state=DISABLED)
-        self.drawing_dir_label.grid(row=0, column=1, padx=(5,10), pady=20)
+        self.drawing_dir_label.grid(row=0, column=2, padx=(0,0), pady=50, sticky="n")
 
         
         search_icon = customtkinter.CTkImage(dark_image=Image.open("./assets/icons/search_white.png"))
         self.search_button = customtkinter.CTkButton(self.drawing_finder_frame, text="",command= self.open_input_dialog_event, anchor="w", image= search_icon, width=1)
-        self.search_button.grid(row=0, column=1, padx=0, pady=20)
-
-        self.pdf_frame = customtkinter.CTkFrame(self.drawing_finder_frame, width=250, height=1000, fg_color="white", border_width=5, border_color= "purple")
-        self.pdf_frame.grid(row = 1, column=1, rowspan = 4,columnspan = 3)
-        self.read_pdf("C:/Users/g/Documents/jobhunt/gerardo.flores-resume.pdf")    
+        self.search_button.grid(row=0, column=3, padx=0, pady=50, sticky="n")
 
         self.drawing_finder_frame.grid_forget()
 
+        #-------------------TEXT FORMATTER---------------------#
+        self.text_formatter_frame = customtkinter.CTkFrame(self, fg_color="gray12")
+        self.text_formatter_frame.grid(row=0, column=1, rowspan = 3, columnspan = 3,sticky="nsew")
+        self.text_formatter_frame.grid_columnconfigure((0,1,2,3,4), weight=0)
+        self.text_formatter_frame.grid_rowconfigure((0,1,2,3,4,5), weight=0)
+
+        self.formatter_label = customtkinter.CTkLabel(self.text_formatter_frame, font = ("Arial", 24),text="Enter notes below\nPreferably in the format shown")
+        self.formatter_label.grid(row=0, column=0, columnspan=3, pady=20)
+
+        self.text_box = customtkinter.CTkTextbox(self.text_formatter_frame, width=500, height=500, border_width=2, border_color= "cyan")
+        self.text_box.insert(index=INSERT ,text="Call# 123456\nWO 02123456\nHD 0001",tags=None)
+        self.text_box.grid(row=1, column=0, columnspan=2, rowspan=3, padx=(20,0))
+
+        self.submit_button = customtkinter.CTkButton(self.text_formatter_frame, text="submit", fg_color="green", text_color="gray12")
+        self.submit_button.grid(row=4, column=1, sticky="e", pady=20)
+
+
+        self.call_label = customtkinter.CTkLabel(self.text_formatter_frame, text="Call#")
+        self.call_label.grid(row=1, column=2, padx=(20,0), sticky="n")
+        self.call_entry = customtkinter.CTkEntry(self.text_formatter_frame)
+        self.call_entry.grid(row=1, column=3, padx=(10,0), sticky="n")
+
+        self.work_order_label = customtkinter.CTkLabel(self.text_formatter_frame, text="Work Order")
+        self.work_order_label.grid(row=2, column=2, padx=(20,0), sticky="n")
+        self.work_order_entry = customtkinter.CTkEntry(self.text_formatter_frame)
+        self.work_order_entry.grid(row=2, column=3, padx=(10,0), sticky="n")
+
+        self.text_formatter_frame.grid_forget()
+
 
         #SETTINGS FRAME
-        self.settings_frame = customtkinter.CTkFrame(self, width=250, fg_color="gray12")
+        self.settings_frame = customtkinter.CTkFrame(self, fg_color="gray12")
         self.settings_frame.grid(row=0, column=1, rowspan = 3, columnspan = 3,padx=(20, 20), pady=(20, 20), sticky="nsew")
         self.settings_frame.grid_rowconfigure((0,1,2), weight = 0)
         self.settings_frame.grid_columnconfigure((0,1,2), weight = 0)
@@ -110,66 +146,15 @@ class App(customtkinter.CTk):
 
 
 
-
-    def read_pdf(self, file_location):
-        doc = fitz.open(file_location)
-
-        zoom = 2
-        mat = fitz.Matrix(zoom, zoom)
-
-        num_pages = 0
-        for p in doc:
-            num_pages += 1
-
-        scrollbar = Scrollbar(self.pdf_frame)
-        canvas = Canvas(self.pdf_frame, yscrollcommand= scrollbar.set,height=1000, width=1000)
-        canvas.pack(side = LEFT, fill = BOTH, expand = 10)
-
-        entry = Entry(self.pdf_frame)
-        label = Label(self.pdf_frame, text="Enter page number to display:")
-
-        def pdf_to_img(page_num):
-            page = doc.load_page(page_num)
-            pix = page.get_pixmap(matrix=mat)
-            return Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        
-        def show_image():
-            try:
-                page_num = int(entry.get()) - 1
-                assert page_num >= 0 and page_num < num_pages
-                im = pdf_to_img(page_num)
-                img_tk = ImageTk.PhotoImage(im)
-                frame = Frame(canvas)
-                panel = Label(frame, image=img_tk)
-                panel.pack(side="bottom", fill="both", expand="yes")
-                frame.image = img_tk
-                canvas.create_window(0, 0, anchor='nw', window=frame)
-                frame.update_idletasks()
-                canvas.config(scrollregion=canvas.bbox("all"))
-            except:
-                pass
-        
-        button = Button(self.pdf_frame, text="Show Page", command=show_image)
-
-
-        label.pack(side=TOP, fill=None)
-        entry.pack(side=TOP, fill=BOTH)
-        button.pack(side=TOP, fill=None)
-
-        entry.insert(0, '1')
-        show_image()
-
-        scrollbar.config(command = canvas.yview)
-        doc.close
-
-
     def reset_buttons(self):
-        self.drawing_button.configure(border_width=0 ,border_color="yellow")
-        self.settings_button.configure(border_width=0 ,border_color="yellow")
+        self.drawing_button.configure(border_width=0 ,border_color="")
+        self.settings_button.configure(border_width=0 ,border_color="")
+        self.formatter_button.configure(border_width=0 ,border_color="")
 
     def reset_frames(self):
         self.drawing_finder_frame.grid_forget()
         self.settings_frame.grid_forget()
+        self.text_formatter_frame.grid_forget()
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -189,25 +174,31 @@ class App(customtkinter.CTk):
         result = drawing_finder(dir, pattern)
 
         if len(result) == 0:
-            print("oops")
+            CTkMessagebox(title="Error", message="File " + pattern + " could not be found", icon="cancel")
         else:
             print(result)
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a drawing number:", title="Drawing Search")
 
-        self.find_drawing(self.drawing_directory, dialog.get_input())
+        self.find_drawing(self.drawing_directory.get(), dialog.get_input())
 
     def load_drawing(self):
         self.reset_frames()
         self.reset_buttons()
-        self.drawing_button.configure(border_width=3 ,border_color="yellow")
+        self.drawing_button.configure(border_width=3 ,border_color="cyan")
         self.drawing_finder_frame.grid(row=0, column=1, rowspan = 5, columnspan = 5,sticky="nsew")
+
+    def load_formatter(self):
+        self.reset_frames()
+        self.reset_buttons()
+        self.formatter_button.configure(border_width=3 ,border_color="cyan")
+        self.text_formatter_frame.grid(row=0, column=1, rowspan = 5, columnspan = 5,sticky="nsew")
 
     def load_settings(self):
         self.reset_frames()
         self.reset_buttons()
-        self.settings_button.configure(border_width=3 ,border_color="yellow")
+        self.settings_button.configure(border_width=3 ,border_color="cyan")
         self.settings_frame.grid(row=0, column=1, rowspan = 3, columnspan = 3, sticky="nsew")
 
 
