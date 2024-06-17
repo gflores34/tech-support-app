58445788fd064e7c8163f5549e8592eca44927a2
Gerardo Flores <63570510+gflores34@users.noreply.github.com>
2024-06-16 20:35:40 -0500

    reformatted text output of find()

--- drawing_finder.py
+++ drawing_finder.py
@@ -1,16 +1,18 @@
-import os, fnmatch
+import os, fnmatch, re
 
 def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
-                result.append(os.path.join(root, name))
+                root = re.sub("\\\\", "/", root)
+                combined = root + "/" + name
+                result.append(combined)
     return result
 
 
 def drawing_finder(dir, pattern):
-
+    
     file_array = find(pattern + ".*", dir)
 
     return file_array
